a09b9fc7e3dd4f1deae71b18ba0c871a355e62a5
Mohammed Saif <mdsaif4online@gmail.com>
2022-01-20 11:44:47 +0530

    3 Populating NewsItem Image and learning about constructor, stae in class based comp

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 export class NewsItem extends Component {
     render() {
-        let {title,description} = this.props;
-        return <div>
+        let {title,description,imageUrl,newsUrl} = this.props;
+        return <div className='my-3'>
             <div className="card" style={{width: "18rem"}}>
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={imageUrl} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <a href="/newsDetail" className="btn btn-primary">Go somewhere</a>
+                    <a href="/newsDetail" className="btn btn-primary btn-sm">Read More</a>
                 </div>
             </div>
         </div>;
