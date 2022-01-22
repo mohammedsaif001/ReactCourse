d40dd27ac09428ba22c9458c70e172cb486bd396
Mohammed Saif <mdsaif4online@gmail.com>
2022-01-22 23:03:14 +0530

    BrouserRouter is changed to HashRouter to deploy on GitHub Pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import React, { useState } from "react";
 import NavBar from "./Components/NavBar";
 import News from "./Components/News";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 
 const App = () => {
