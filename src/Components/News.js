98e9e8a66cdb0f4e05385f83870daffbb40147a7
Mohammed Saif <mdsaif4online@gmail.com>
2022-01-22 15:54:31 +0530

    11 Refactoring News Component

--- src/Components/News.js
+++ src/Components/News.js
@@ -12,7 +12,7 @@ export class News extends Component {
     }
 
     static propTypes = {
-      country : PropTypes.number,
+      country : PropTypes.string,
       category : PropTypes.string
     }
 
@@ -26,49 +26,35 @@ export class News extends Component {
         }
     }
 
-    async componentDidMount(){  //Executes after Render
-        console.log("Component Did Mount"); //Runs 3rd
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=8de92e1fb9b4472992bb1ae2c00cc11b&page=1&pageSize=${this.props.pageSize}`;
+    async updateNews(){
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=8de92e1fb9b4472992bb1ae2c00cc11b&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+      this.setState({loading:true});
         let data = await fetch(url);
         let parsedData = await data.json();
-        console.log(parsedData);
         this.setState({
           articles:parsedData.articles,
-          totalArticles : parsedData.totalResults
+          totalArticles : parsedData.totalResults,
+          loading : false
         })
     }
     
+    async componentDidMount(){  //Executes after Render
+       this.updateNews();
+    }
+
+    
      handlePrevClick = async()=>{
       console.log("Previous");
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=8de92e1fb9b4472992bb1ae2c00cc11b&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-      this.setState({loading : true});
-      let data = await fetch(url);
-      let parsedData = await data.json();
-     
-    this.setState({
-      page : this.state.page+1,
-      articles : parsedData.articles,
-      loading : false
-    })
+      this.setState({page : this.state.page-1});
+      this.updateNews();
+      
     }
 
      handleNextClick = async()=>{
       console.log("Next");
 
-      if( ! (this.state.page+1 > Math.ceil(this.state.totalArticles/this.props.pageSize))){
-
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=8de92e1fb9b4472992bb1ae2c00cc11b&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading : true});
-        let data = await fetch(url);
-        let parsedData = await data.json();
-       
-      this.setState({
-        page : this.state.page+1,
-        articles : parsedData.articles,
-        loading : false
-      })
-      }
-      
+      this.setState({page : this.state.page+1});
+      this.updateNews();
     }
 
     
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
         let {title,description,imageUrl,newsUrl,author,date,source} = this.props;
         return <div className='my-3'>
             <div className="card" >
-            <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>{source}</span>
+            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>{source}</span>
                 <img src={imageUrl?imageUrl:`https://bitsofco.de/content/images/2018/12/broken-1.png`} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{title}...</h5>
