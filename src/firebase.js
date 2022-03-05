a17b60f71c87b767ecfb069a6ac05e1a7d143ae3
mohammedsaif001 <mdsaif4online@gmail.com>
2022-03-05 23:00:09 +0530

    Changing API

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCwrMTeV8Zud38stMpmG3DxvmaGUU3It2Y",
+  authDomain: "news-flash-292a8.firebaseapp.com",
+  projectId: "news-flash-292a8",
+  storageBucket: "news-flash-292a8.appspot.com",
+  messagingSenderId: "62145844122",
+  appId: "1:62145844122:web:705bc89041fef6d767a023",
+  measurementId: "G-G1YGFR8NZY",
+};
