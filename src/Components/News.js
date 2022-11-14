399fb3eb729d803dc5f6b5dc8d3d15febfef9d67
mohammedsaif-nviera <mohammedsaif@nviera.com>
2022-11-14 11:20:54 +0530

    Fixing Error

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState}  from 'react';
+import React, { useEffect, useState } from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
@@ -7,98 +7,98 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 
 const News = (props) => {
 
-  const capitalizeFirstLetter = (string) =>{
+  const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-    const [articles, setArticles] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [page, setPage] = useState(1);
-    const [totalArticles, setTotalArticles] = useState(0);
-    
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalArticles, setTotalArticles] = useState(0);
 
-    const updateNews = async () =>{
-      props.setProgress(10);
-      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-        setLoading(true);
-        let data = await fetch(url);
-        props.setProgress(30);
-        let parsedData = await data.json();
-        props.setProgress(70);
 
-        setArticles(parsedData.articles);
-        setTotalArticles(parsedData.totalResults);
-        setLoading(false);
+  const updateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
+    let data = await fetch(url);
+    props.setProgress(30);
+    let parsedData = await data.json();
+    props.setProgress(70);
 
-        props.setProgress(100);
-    }
-    
-    useEffect(() => {
-      document.title = `${capitalizeFirstLetter(props.category)} - News Flash`;
-      updateNews();
-      //eslint-disable-next-line
-    }, []);
-    
-       
+    setArticles(parsedData.articles);
+    setTotalArticles(parsedData.totalResults);
+    setLoading(false);
 
-    const  fetchMoreData = async() => {
-      
-      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
-      setPage(page+1);
-      
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        setArticles(articles.concat(parsedData.articles));
-        setLoading(false);
-        setTotalArticles(parsedData.totalResults);
-        
-    };
+    props.setProgress(100);
+  }
 
-    
-     
-      
-        return (
-        <>
-          <h1 className="text-center" style={{margin:"25px",marginTop:'90px'}}>NEWS FLASH  - TOP HEADLINES [{capitalizeFirstLetter(props.category)}]</h1>
-          {loading && <Spinner/>}
-            <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalArticles}
-          loader={<Spinner/>}
-        >
-          <div className="container">
+  useEffect(() => {
+    document.title = `${capitalizeFirstLetter(props.category)} - News Flash`;
+    updateNews();
+    //eslint-disable-next-line
+  }, []);
 
-          
-            <div className="row">
-            {articles.map((element)=>{
-                return <div className="col-md-4"  key={element.url}>
-                     <NewsItem title={ element.title ? element.title.slice(0,45):""} description={ element.description ? element.description.slice(0,88) : ""} imageUrl={element.urlToImage} newsUrl = {element.url} author = {element.author} date = {element.publishedAt} source={element.source.name}/>
 
-                     {/* .slice() is used to limit number of characters here starting from 0 to 88 characters */}
-                     </div>
+
+  const fetchMoreData = async () => {
+
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
+    setPage(page + 1);
+
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles(articles.concat(parsedData.articles));
+    setLoading(false);
+    setTotalArticles(parsedData.totalResults);
+
+  };
+
+
+
+
+  return (
+    <>
+      <h1 className="text-center" style={{ margin: "25px", marginTop: '90px' }}>NEWS FLASH  - TOP HEADLINES [{capitalizeFirstLetter(props.category)}]</h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles?.length}
+        next={fetchMoreData}
+        hasMore={articles?.length !== totalArticles}
+        loader={<Spinner />}
+      >
+        <div className="container">
+
+
+          <div className="row">
+            {articles.map((element) => {
+              return <div className="col-md-4" key={element.url}>
+                <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+
+                {/* .slice() is used to limit number of characters here starting from 0 to 88 characters */}
+              </div>
 
             })}
           </div>
-          </div>
+        </div>
 
-          </InfiniteScroll>
-       
-        </>
-        )
-    
-  }
+      </InfiniteScroll>
+
+    </>
+  )
+
+}
 
 
-  News.defaultProps = {
-    country : 'in',
-    pageSize : 8,
-    category : 'general'
-  }
+News.defaultProps = {
+  country: 'in',
+  pageSize: 8,
+  category: 'general'
+}
 
-  News.propTypes = {
-    country : PropTypes.string,
-    category : PropTypes.string
-  }
+News.propTypes = {
+  country: PropTypes.string,
+  category: PropTypes.string
+}
 
 export default News;
